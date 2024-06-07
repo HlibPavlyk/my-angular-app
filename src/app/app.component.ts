1b611f7f97bab7277c68b873c2b6bccae81aa444
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-07 14:18:46 +0300

    feat: add task component
    - add new nested component
    - edit tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import { DUMMY_USERS} from "./dummy-users";
-import {TaskComponent} from "./task/task.component";
+import {TasksComponent} from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-angular-app';
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find(user => user.id === this.selectedUserId)!;
