2a7412bcdae405c62d5ae185538626f5da6aac8f
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-09 13:07:31 +0300

    feat: add new task component
    - that way you can add new tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from "./task/task.component";
+import {NewTaskComponent} from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
@@ -13,6 +15,8 @@ import {TaskComponent} from "./task/task.component";
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input() name?: string;
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
@@ -46,4 +50,13 @@ export class TasksComponent {
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
   }
+
+  onStartAddTask() {
+    this.isAddingTask= true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask= false;
+  }
+
 }
